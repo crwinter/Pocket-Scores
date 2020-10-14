274adaee67242809f085feb7f70366b1c9626da5
gobelluncg <69978112+gobelluncg@users.noreply.github.com>
2020-10-14 16:38:35 -0400

    Add files via upload

--- src/PocketScores/PocketScoresFXController.java
+++ src/PocketScores/PocketScoresFXController.java
@@ -8,6 +8,14 @@ package PocketScores;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -22,6 +30,23 @@ public class PocketScoresFXController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+    
+    @FXML
+    public Button NFL;
+
+    @FXML
+    private void goToNFL(ActionEvent event){
+        try{
+            Stage stage = (Stage) NFL.getScene().getWindow();
+            stage.close();
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("NFLListView.fxml"));
+            stage.setScene(new Scene(root, 450, 450));
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }    
     
 }
--- src/PocketScores/PocketScoresMain.java
+++ src/PocketScores/PocketScoresMain.java
@@ -3,20 +3,30 @@ package PocketScores;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
 
 /**
  *
  * @author brandonbanner
  */
 public class PocketScoresMain extends Application {
+    
+    public static void main(String[] args){
+        launch(args);
+    }
 
-    @Override
+    //@Override
+    @FXML
     public void start(Stage stage) throws Exception {
-     
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("PocketScoresFXML.fxml"));
+        stage.setScene(new Scene(root, 450, 450));
+        stage.show();
     }
-        public static void main(String[] args) {
-            launch(args);
-    }
+    
 }
 
 

bf939bfe1ec8efc10b76867cf3330a2e4dc441b9
Brandon Banner <46693266+brandonbanner@users.noreply.github.com>
2020-10-13 23:40:04 -0400

    Add files via upload

---
+++ src/PocketScores/PocketScoresMain.java
@@ -0,0 +1,22 @@
+
+package PocketScores;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author brandonbanner
+ */
+public class PocketScoresMain extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+     
+    }
+        public static void main(String[] args) {
+            launch(args);
+    }
+}
+
+
